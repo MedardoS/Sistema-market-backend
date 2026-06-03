ac2654414a5865ff4c46699f2437668a7a3656ba
medardoSanchez <medardosnchez09@gmail.com>
2026-06-03 16:00:34 -0400

    Add

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
 
-import {
-  createProduct,
-} from "../controllers/productController.js";
+import { createProduct } from "../controllers/productController.js";
 
 const router = Router();
 
-router.post(
-  "/",
-  createProduct
-);
+router.post("/", createProduct);
 
 export default router;
