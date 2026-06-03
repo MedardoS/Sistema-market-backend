d148ae043e6e0a6d23a043d772730d8e40164e90
medardoSanchez <medardosnchez09@gmail.com>
2026-06-03 05:38:25 -0400

    Update db connection

--- src/db/db.js
+++ src/db/db.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "sistema_market",
-  password: "lume",
-  port: 5432,
+  connectionString:
+    process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
